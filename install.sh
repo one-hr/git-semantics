3e3b9f6f1ed3eefa42be5969b9d57196745f73f7
Xiaoqin.Lin <1102558795@qq.com>
2022-09-18 22:27:21 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 rm -rf .git/hooks/pre-commit
 rm -rf .git/hooks/commit-msg
 
-curl --fail -o .git/hooks/commit-msg https://raw.githubusercontent.com/lin-xiaoqin/git-semantics/master/pre-commit \
+curl --fail -o .git/hooks/pre-commit https://raw.githubusercontent.com/lin-xiaoqin/git-semantics/master/pre-commit \
   && chmod 500 .git/hooks/pre-commit
 
 curl --fail -o .git/hooks/commit-msg https://raw.githubusercontent.com/lin-xiaoqin/git-semantics/master/commit-msg \
